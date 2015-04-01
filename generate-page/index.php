6613c901a546483c825d40c870522d20f8128d68
Pete Goodman <thegingerbloke@users.noreply.github.com>
2015-04-01 16:26:15 +0100

    Making API calls https

--- generate-page/index.php
+++ generate-page/index.php
@@ -1,5 +1,5 @@
 <?php
-  
+
 
   // debug
   debug("starting...");
@@ -32,7 +32,7 @@
 
 
   // create a flickr app
-  // http://www.flickr.com/services/api/auth.howto.web.html
+  // https://www.flickr.com/services/api/auth.howto.web.html
 
 
 
@@ -40,49 +40,49 @@
   // register a flickr app, get a flickr api key & secret
   if (!$conf['flickr']['api_key'] || !$conf['flickr']['api_secret']) {
     debug("error: register a flickr app for an api key (and secret)");
-    debug("visit: http://www.flickr.com/services/apps/create/");
+    debug("visit: https://www.flickr.com/services/apps/create/");
     exit;
   }
 
 
   // step 1
   // calculate flickr frob
-  // step 1: http://www.flickr.com/services/auth/?api_key=[]&perms=read&api_sig=[]
+  // step 1: https://www.flickr.com/services/auth/?api_key=[]&perms=read&api_sig=[]
   if (!$conf['flickr']['frob']) {
     $api_sig = md5($conf['flickr']['api_secret']."api_key".$conf['flickr']['api_key']."permsread");
-    $frobURL = "http://www.flickr.com/services/auth/?api_key=".$conf['flickr']['api_key']."&perms=read&api_sig=".$api_sig;
+    $frobURL = "https://www.flickr.com/services/auth/?api_key=".$conf['flickr']['api_key']."&perms=read&api_sig=".$api_sig;
     debug("error: no frob");
     debug("visit: ".$frobURL);
     exit;
   }
 
 
-  
+
   // step 2
   // calculate flickr auth_token
   if (!$conf['flickr']['auth_token']) {
     $api_sig = md5($conf['flickr']['api_secret']."api_key".$conf['flickr']['api_key']."formatjsonfrob".$conf['flickr']['frob']."methodflickr.auth.getTokennojsoncallback1");
-    $tokenURL = "http://api.flickr.com/services/rest/?method=flickr.auth.getToken&api_key=".$conf['flickr']['api_key']."&frob=".$conf['flickr']['frob']."&format=json&nojsoncallback=1&api_sig=".$api_sig;
+    $tokenURL = "https://api.flickr.com/services/rest/?method=flickr.auth.getToken&api_key=".$conf['flickr']['api_key']."&frob=".$conf['flickr']['frob']."&format=json&nojsoncallback=1&api_sig=".$api_sig;
     $token = fetch($tokenURL);
     debug("error: no auth_token");
     debug("your auth_token: " . $token->auth->token->content);
     exit;
   }
 
-  
+
   // fetch flickr sets
   debug("fetching flickr sets...");
   $api_sig = md5($conf['flickr']['api_secret']."api_key".$conf['flickr']['api_key']."auth_token".$conf['flickr']['auth_token']."formatjsonmethodflickr.photosets.getListnojsoncallback1");
-  $collectionsURL = "http://api.flickr.com/services/rest/?method=flickr.photosets.getList&api_key=".$conf['flickr']['api_key']."&auth_token=".$conf['flickr']['auth_token']."&format=json&nojsoncallback=1&api_sig=".$api_sig;
+  $collectionsURL = "https://api.flickr.com/services/rest/?method=flickr.photosets.getList&api_key=".$conf['flickr']['api_key']."&auth_token=".$conf['flickr']['auth_token']."&format=json&nojsoncallback=1&api_sig=".$api_sig;
   $collections = fetch($collectionsURL);
-  
-  $collectionCount = $collections->photosets->total;  
+
+  $collectionCount = $collections->photosets->total;
 
   if (!is_numeric($collectionCount) || $collectionCount < 0) {
     debug("error: no collections found");
     exit;
   } else {
-    debug("found " . $collectionCount . " collections");    
+    debug("found " . $collectionCount . " collections");
   }
 
 
@@ -92,15 +92,15 @@
     $title = $photoset->title->_content;
     debug("fetching photos for set: " . $title);
     $api_sig = md5($conf['flickr']['api_secret']."api_key".$conf['flickr']['api_key']."auth_token".$conf['flickr']['auth_token']."extrasdescriptionformatjsonmethodflickr.photosets.getPhotosnojsoncallback1photoset_id".$photoset->id."");
-    $photosURL = "http://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=".$conf['flickr']['api_key']."&photoset_id=".$photoset->id."&extras=description&format=json&nojsoncallback=1&auth_token=".$conf['flickr']['auth_token']."&api_sig=".$api_sig;
+    $photosURL = "https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=".$conf['flickr']['api_key']."&photoset_id=".$photoset->id."&extras=description&format=json&nojsoncallback=1&auth_token=".$conf['flickr']['auth_token']."&api_sig=".$api_sig;
     $photos = fetch($photosURL);
-    
+
     foreach ($photos->photoset->photo as $photo) {
       $images[] = array(
         "title" => $photo->title,
         "description" => $photo->description->_content,
         "location" => $title,
-        "src" => "http://farm".$photo->farm.".staticflickr.com/".$photo->server."/".$photo->id."_".$photo->secret."_b.jpg"
+        "src" => "https://farm".$photo->farm.".staticflickr.com/".$photo->server."/".$photo->id."_".$photo->secret."_b.jpg"
       );
     }
     debug("added " . $photos->photoset->total . " images");
@@ -117,9 +117,9 @@
 
   // loop through each image
   foreach ($images as $key => $image) {
-    
+
     $imageData = $imageTemplate;
-    
+
     // loop through each image value
     foreach($image as $imageKey => $imageValue) {
       $imageData = str_replace("{{".$imageKey."}}", $imageValue, $imageData);
@@ -140,7 +140,7 @@
   }
 
   $page .= $footer;
-  
+
 
 
   // save new index page
@@ -149,7 +149,7 @@
   } else {
     debug("error: could not create page");
   }
-  
+
 
 
 
@@ -162,11 +162,11 @@ function debug($message){
 
 // flickr/curl
 function fetch($url) {
-  $ch = curl_init();  
-  curl_setopt ($ch, CURLOPT_URL, $url);  
-  curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);  
-  curl_setopt ($ch, CURLOPT_CONNECTTIMEOUT, 5);  
-  $file_contents = curl_exec($ch);  
+  $ch = curl_init();
+  curl_setopt ($ch, CURLOPT_URL, $url);
+  curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt ($ch, CURLOPT_CONNECTTIMEOUT, 5);
+  $file_contents = curl_exec($ch);
   curl_close($ch);
   return json_decode($file_contents);
 }
